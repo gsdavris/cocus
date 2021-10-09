aa8a6610b6977bc883f7b268f3775c39f02db73b
gsdavris <g.s.davris@gmail.com>
2021-10-09 16:48:33 +0300

    fixing post card

--- src/components/blog/Post.jsx
+++ src/components/blog/Post.jsx
@@ -6,16 +6,18 @@ import {sanitize} from '../../utils/miscellaneous';
 const Post = ( {post} ) => {
 
 	return (
-		<div className="mb-8">
+		<div className="mb-8 shadow-lg hover:shadow rounded-lg break-words">
 			<figure className="overflow-hidden mb-4">
-				<Image { ...post?.featuredImage?.node } width="400" height="225" layout="fill" containerClassNames="w-96 sm:-w-600px md:w-400px h-56 sm:h-338px md:h-225px" title={post?.title ?? ''}/>
+				<Image { ...post?.featuredImage?.node } width="400" height="225" layout="fill" containerClassNames="w-96 sm:-w-600px md:w-400px h-56 sm:h-338px md:h-225px rounded-t-lg" title={post?.title ?? ''}/>
 			</figure>
+			<blockquote class="relative p-8 mb-4">
 			<Link href={`/blog/${post?.slug}/`}>
 				<a>
 					<h2 className="font-bold mb-3 text-lg hover:text-gold" dangerouslySetInnerHTML={{__html: sanitize( post?.title ?? '' )}}/>
 				</a>
 			</Link>
 			<div dangerouslySetInnerHTML={{__html: sanitize( post?.excerpt ?? '' )}}/>
+			</blockquote>
 		</div>
 	);
 };
